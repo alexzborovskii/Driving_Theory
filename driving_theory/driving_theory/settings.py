8fceda76dedb7ce52cd6b4a7f02157da201cfe1a
alexzborovskii <alexzborovskii@gmail.com>
2023-10-21 21:18:11 +0300

    mimetype debug condition changed

--- driving_theory/driving_theory/settings.py
+++ driving_theory/driving_theory/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY"),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-if not DEBUG:
+if DEBUG:
     import mimetypes
     mimetypes.add_type("application/javascript", ".js", True)
     mimetypes.add_type("text/css", ".css", True)
