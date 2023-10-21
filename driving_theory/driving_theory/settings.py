41035d2c21ce8fdd3ac48095b045c7d76e85d975
alexzborovskii <alexzborovskii@gmail.com>
2023-10-21 20:34:16 +0300

    secret key added

--- driving_theory/driving_theory/settings.py
+++ driving_theory/driving_theory/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n52#-y&z+q3v0g$5d7f+!g7mie4_&plsg%vf9v45cia=nsw*a*'
+SECRET_KEY = os.getenv("SECRET_KEY"),
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
