f26a3ad86c2fabc0089cbb05077fabe5a0d577fe
alexzborovskii <alexzborovskii@gmail.com>
2023-10-23 12:43:32 +0300

    url settings change for static files

--- driving_theory/driving_theory/urls.py
+++ driving_theory/driving_theory/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from exam.views import HomePageView
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +26,6 @@ urlpatterns = [
     path('exam/', include('exam.urls') ),
     path('', HomePageView.as_view(), name='index'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
