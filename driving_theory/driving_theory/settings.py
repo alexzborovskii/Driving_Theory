05a5c84535cd1a1eacebb1cd7e3d1fde684c6bf4
alexzborovskii <alexzborovskii@gmail.com>
2023-10-24 12:39:13 +0300

    cors whitelist = all

--- driving_theory/driving_theory/settings.py
+++ driving_theory/driving_theory/settings.py
@@ -150,10 +150,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = True  # Set this to True to allow all origins
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:8000',
-    'http://127.0.0.1:8000',# Add your allowed front-end origins here
-]
+CORS_ORIGIN_WHITELIST = ["*"]
 
 LOGGING = {
     'version': 1,
