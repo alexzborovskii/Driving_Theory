a9d2501260a6be998de3779e4832028f029c2bbd
alexzborovskii <alexzborovskii@gmail.com>
2023-10-24 13:53:28 +0300

    url in staticfiles script changed

--- driving_theory/staticfiles/script.js
+++ driving_theory/staticfiles/script.js
@@ -11,7 +11,7 @@ let curr_question = 0;
 
 async function getAllLanguages() {
     try {
-        const url = "http://127.0.0.1:8000/exam/languages/";
+        const url = "https://driving-theory.onrender.com/exam/languages/";
         const response = await fetch(url);
         if (!response.ok) {
             throw new Error("Couldn`t get languages")
